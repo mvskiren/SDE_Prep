060042f5b37fb9dafaece50479bcc996fd764819
mvskiren <kiranmvs07@gmail.com>
2021-02-10 12:29:25 +0530

    Added Demistifying Arrays

---
+++ Algorithms/miscellaneous.java
@@ -0,0 +1,37 @@
+// Demistifying the arrays 
+
+public class digital1 {
+
+	public static void main(String[] args) {
+      
+		int arr[]= {23,434,334,4,3,43,3,3,3,3};
+		String str="hellothereis";
+		int k=5;
+	
+// Basic loop 
+//		for(int i=0;i<arr.length-1;i++) {
+//			
+//			
+//			for(int j=i+1;j<arr.length;j++)
+//			{
+//				
+//				System.out.println(arr[j]); 
+//			}
+//			
+//		}
+//	
+//	Substring iterate loop with k window
+//     for(int i=0;i<str.length()-k+1;i++)
+//     {  
+//    	 
+//    	 String s=" ";
+//    	 for(int j=i; j<i+k; j++) {  
+// 		 s=s+Character.toString(str.charAt(j));
+//    	 }
+//    	 System.out.println(s);
+//   
+//     }	
+	}
+		
+
+}
