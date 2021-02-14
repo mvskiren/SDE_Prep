5a645d62b915c21f76693e423385b0a186105fd0
mvskiren <kiranmvs07@gmail.com>
2021-02-14 20:27:04 +0530

    Leetcode-28. Implement strStr()

--- Algorithms/Leetcode/strStr().java
+++ Algorithms/Leetcode/strStr().java
@@ -30,3 +30,21 @@ public int strStr(String haystack, String needle) {
 
 Time complexity: O(n*m) wrost case;
 Space : 0(1); //No extra space required;
+
+/******************************************************************************** */
+// Second approach
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.isEmpty())
+            return 0;
+        if(needle.length() > haystack.length())
+            return -1;
+        if(needle.length()==haystack.length() && needle.equals(haystack))
+            return 0;
+        for(int i=0;i<=haystack.length()-needle.length();i++) {
+            if(haystack.charAt(i) == needle.charAt(0) && haystack.substring(i, i+needle.length()).equals(needle))
+                return i;
+        }
+        return -1;
+    }
+}
